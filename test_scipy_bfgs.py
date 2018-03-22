1be32f3ee575ba5e46a936f1b0ea4ca9c0953b54
Simon Clément <simon.clement@ensimag.fr>
2018-03-22 19:44:10 +0100

    ajout test bfgs

---
+++ test_scipy_bfgs.py
@@ -0,0 +1,19 @@
+import scipy.optimize as opti
+import numpy as np
+
+def f(x, m):
+    """x is passed as a 1D-array : needed to be reshaped
+        m is the number of lines in the matrix Phi
+    """
+    return x[0] + 2*x[1] +3*x[2]
+
+def grad(x, m):
+    """x is passed as a 1D-array : needed to be reshaped
+        m is the number of lines in the matrix Phi
+    """
+    return [1, 2, 3, 0, 0, 0]
+
+x0 = np.ravel(np.array([[4,3,1],[2,3,4]]))
+print("avec x0=0:", f(x0, 2), grad(x0, 2))
+print(opti.minimize(f, x0, args=(2), jac=grad, method='BFGS'))
+
