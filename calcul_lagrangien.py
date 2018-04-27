788b75d5abfacc357ab07b045cacc43730f2aaec
Simon Clément <simon.clement@ensimag.fr>
2018-04-27 10:43:47 +0200

    optimisations légères pour comparaisons de perfs

--- calcul_lagrangien.py
+++ calcul_lagrangien.py
@@ -3,7 +3,7 @@ from itertools import repeat
 import concurrent.futures
 from res_direct import res_direct, calculer_M, calculer_K, res_direct_one_step, res_direct_tridiagonal
 
-beta = 0#000001
+beta = 1
 
 def calcul_error(Phi, m, ensemble_apprentissage):
     Phi = np.reshape(Phi, (m, -1))
@@ -26,14 +26,17 @@ def calcul_lagrangien(Phi, m, ensemble_apprentissage):
             Phi, ensemble_apprentissage)
     return ret
 
-
-def calcul_gradient(Phi, m, ensemble_apprentissage):
+disp = False
+def calcul_gradient(Phi, m, ensemble_apprentissage, display=False):
     """ renvoie le gradient du lagrangien en Phi,
     avec l'ensemble d'apprentissage"""
     Phi = np.reshape(Phi, (m, -1))
     diagonale = [1 - np.vdot(Phi[:,i], Phi[:,i]) \
             for i in range(Phi.shape[1])]
     x0 = - 2* beta * Phi @ np.diag(diagonale)
+    if display:
+        disp = True
+        print("x0 : ", x0)
     return np.ravel(calcul_forall_k(calcul_sous_gradient, x0, 
             Phi, ensemble_apprentissage))
 
@@ -48,6 +51,7 @@ def calcul_sous_lagrangien(Phi, u_k, f_k, alpha_k, lambda_k, mu_k,
             in zip(u_k, alpha_k)]
     diff.pop(0)
     diff.pop(-1) # pas opti mais c'est pour le bien du test
+    # on pop les valeurs extremes pour avoir un gradient exact (?)
     diff = np.array(diff)
     return np.vdot(diff,diff) / 2
 
@@ -58,8 +62,8 @@ def sous_calcul_erreur(Phi, u_k, f_k, alpha_k, lambda_k, mu_k,
         } 
         H n'est *PAS* pris en compte
     """
-    return [max(abs(u_n - Phi @ alpha_n)) for u_n, alpha_n \
-            in zip(u_k, alpha_k)][0]
+    return max([max(abs(u_n - Phi @ alpha_n)) for u_n, alpha_n \
+            in zip(u_k, alpha_k)])
 
 
 def calcul_sous_gradient(Phi, u_k, f_k, alpha_k, lambda_k, mu_k, 
@@ -70,6 +74,9 @@ def calcul_sous_gradient(Phi, u_k, f_k, alpha_k, lambda_k, mu_k,
     """
     ret = 0
     if len(alpha_k[0].shape) == 1:
+        # si on est en dimension 1, il faut redimensionner
+        # les tableaux pour que les produits
+        # matriciels soient valides
         for n in range(len(lambda_k)):
             alpha_k[n] = alpha_k[n].reshape(
                     (alpha_k[n].shape[0], -1))
@@ -78,6 +85,7 @@ def calcul_sous_gradient(Phi, u_k, f_k, alpha_k, lambda_k, mu_k,
             u_k[n] = u_k[n].reshape(
                     (u_k[n].shape[0], -1))
 
+        # la taille d'alpha est (celle de lambda) +1
         alpha_k[-1] = alpha_k[-1].reshape(
                 (alpha_k[-1].shape[0], -1))
         mu_k = mu_k.reshape((mu_k.shape[0], -1))
@@ -92,18 +100,19 @@ def calcul_sous_gradient(Phi, u_k, f_k, alpha_k, lambda_k, mu_k,
         ret_n += np.outer(K@Phi@alpha_k[n+1],lambda_k[n])
         ret_n += np.outer(K.T@Phi@lambda_k[n],alpha_k[n+1])
         ret_n -= np.outer(f_k[n],lambda_k[n])
-        # ici c'est bien all_f[n], car l'utilisateur envoie
+        # ici c'est bien f_k[n], car l'utilisateur envoie
         # (normalement) pas f0. donc all_f[n] = f_{n+1}
         ret += ret_n
-    #ret += M@Phi@(alpha_k[0]@mu_k.T + mu_k@alpha_k[0].T)
-    ret_n -= Phi@alpha_k[0] @ alpha_k[0].T
-    ret_n += u_k[0] @ alpha_k[0].T
+    
+    ret += M@Phi@(alpha_k[0]@mu_k.T + mu_k@alpha_k[0].T)
+    ret -= Phi@alpha_k[0] @ alpha_k[0].T
+    ret += u_k[0] @ alpha_k[0].T
+    
     ret -= np.outer(M@u_k[0],mu_k)
     return ret
 
 
-
-def func_parallel(tuple_data_func_Phi):
+def call_func_with_computed_data(tuple_data_func_Phi):
     """ fait le travail de chercher les alpha/lambda/mu
     pour un élément de la base d'apprentissage tuple_data.
     appelle func.
@@ -133,13 +142,14 @@ def calcul_forall_k(func, x0, Phi, ensemble_apprentissage):
         on pourra par la suite ajouter H dans le tuple...
         On pourra aussi faire en sorte que K ne soit plus
         constant : tout ce travail est à faire dans
-        func_parallel
+        call_func_with_computed_data
 
     """
     # \beta\Phi diag(...) est le seul element qui depend
     # pas de l'ensemble d'apprentissage
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        x0 += sum(executor.map(func_parallel, zip(ensemble_apprentissage, repeat(func), repeat(Phi))))
+        x0 += sum(executor.map(call_func_with_computed_data, 
+            zip(ensemble_apprentissage, repeat(func), repeat(Phi))))
     return x0
 
 def calcul_alpha(Phi, M, K, u0, all_f, dt):
@@ -155,20 +165,25 @@ def calcul_alpha(Phi, M, K, u0, all_f, dt):
                     de n=0 à n_max.
     """
     Phi_T_M_Phi = Phi.T@M@Phi
-    alpha = [np.linalg.lstsq(Phi_T_M_Phi, 
-        Phi.T@M@u0, rcond=None)[0]]
+    try:
+        sec_membre = np.reshape(Phi.T@M@u0, (Phi_T_M_Phi.shape[0], 1))
+        alpha = [np.linalg.solve(Phi_T_M_Phi, sec_membre)]
+    except(np.linalg.linalg.LinAlgError):
+        alpha = [np.linalg.lstsq(Phi_T_M_Phi, 
+            Phi.T@M@u0, rcond=None)[0]]
     # seconde equation = Phi^T M Phi \alpha0 = \Phi^T M u0
     Phi_T_M_Phi_dt = Phi_T_M_Phi / dt
+    Phi_T_K_Phi = Phi.T@K@Phi
     #on fait zero + pour être sur d'avoir un nparray de dim2
     for fn in all_f:
+        second_membre = Phi.T @ fn + Phi_T_M_Phi_dt @ alpha[-1]
         try:
-            second_membre = Phi.T @ fn + Phi_T_M_Phi_dt @ alpha[-1]
+            alpha.append(np.linalg.solve(Phi_T_M_Phi_dt + \
+                    Phi_T_K_Phi, second_membre))
+        except(np.linalg.linalg.LinAlgError):
             alpha.append(np.linalg.lstsq(Phi_T_M_Phi_dt + \
                     Phi.T@K@Phi, second_membre, rcond=None)[0])
-        except(np.linalg.linalg.LinAlgError):
-            print("erreur : singular matrix")
-            print(Phi)
-            raise
+            print("warning : singular matrix")
     return alpha
 
 
@@ -180,12 +195,13 @@ def calcul_lambda(Phi, K, M, u, alpha, dt, n_max):
     Phi_T_M_Phi = Phi.T@M@Phi
     for n in range(n_max-1, 0, -1):
         try:
+            lambda_ret[n-1] = np.linalg.solve(Phi_T_M_Phi + \
+                    dt * Phi.T@K.T@Phi, Phi_T_M_Phi@lambda_ret[n] + \
+                    dt * Phi.T @ (u[n] - Phi @ alpha[n]))
+        except(np.linalg.linalg.LinAlgError):
             lambda_ret[n-1] = np.linalg.lstsq(Phi_T_M_Phi + \
                     dt * Phi.T@K.T@Phi, Phi_T_M_Phi@lambda_ret[n] + \
                     dt * Phi.T @ (u[n] - Phi @ alpha[n]), rcond=None)[0]
-        except(np.linalg.linalg.LinAlgError):
-            print("erreur : singular matrix : ")
-            print(Phi)
-            raise
+            print("warning : singular matrix : ")
     return lambda_ret
 
--- data_ass_with_pod.py
+++ data_ass_with_pod.py
@@ -1,9 +1,13 @@
+from itertools import repeat
+import concurrent.futures
+import matplotlib.pyplot as plt
 import math
 from netCDF4 import Dataset
-from calcul_lagrangien import calcul_lagrangien,calcul_gradient, calcul_error
+from calcul_lagrangien import calcul_lagrangien,calcul_gradient, calcul_error, call_func_with_computed_data, calcul_forall_k
 from res_direct import calculer_K
 import numpy as np
 import scipy.optimize as opti
+from export_img import export_img
 
 rootgrp = Dataset("donnees.nc", "r+", format="NETCDF4")
 """
@@ -46,31 +50,85 @@ for tk in range(int(len(rootgrp["zhalf"][:,0,0,0])/2),
         # b est la valeur de u en h_max
         b = rootgrp["temp"][t,-1,0,0]
         all_f += [np.concatenate(([a], [0] * (nb_h-2), [b]))] 
+
     discretisation_h = rootgrp["zhalf"][tk,:,0,0]
     u0 = rootgrp["temp"][tk,:,0,0]
     nu_1_2 = rootgrp["kz"][tk,1:,0,0] # on enlève la premiere valeur
+    """
+    if tk == int(len(rootgrp["zhalf"][:,0,0,0])/2):
+        plt.plot(discretisation_h, u0, 'r')
+    elif tk == int(len(rootgrp["zhalf"][:,0,0,0])/2)+500:
+        plt.plot(discretisation_h, u0, 'g')
+    elif tk == int(len(rootgrp["zhalf"][:,0,0,0])/2)+1000:
+        plt.plot(discretisation_h, u0, 'b')
+    """
     # (normalement faudrait prendre nu en les points intermédiaires
     K = calculer_K(discretisation_h, nu_1_2)
     ensemble_test += [(u0, K, all_f, dt)]
 
 
+#plt.xscale("log")
+
+# export_img("hauteur(m)", "T°", "figures_optimisation/profil_u0.png")
+
+
+
 # ------------ Calcul de la base POD pour Phi
 from modred.pod import compute_POD_matrices_direct_method
 vecs = rootgrp["temp"][:,:,0,0].T
-# ceci etait le calcul de pod...
-erreur_moyenne = []
-erreur_moyenne_only_pod = []
+erreur_max = []
+erreur_max_residuel = []
 Phi = None
 
 try:
-r   for i in range(2,80):
+   for i in range(2,10):
+        print("i =",i)
         Phi = compute_POD_matrices_direct_method(vecs, range(i))[0]
         Phi = np.ravel(Phi)
-        erreur_moyenne_only_pod += [calcul_error(Phi, nb_h, ensemble_apprentissage)/len(ensemble_apprentissage)]
         res_opti = opti.minimize(calcul_lagrangien, Phi, jac=calcul_gradient, args=(nb_h, ensemble_apprentissage), method='BFGS', options={'disp': True})
-        erreur_moyenne += [calcul_error(Phi, nb_h, ensemble_apprentissage)/len(ensemble_apprentissage)]
+
+        plt.clf()
+        color = 'r+'
+        Phi = np.reshape(res_opti.x, (-1, i))
+        def erreur_norme_inf(Phi, u_k, f_k, alpha_k, *param):
+            return [max([max(abs(u_n - Phi @ alpha_n)) \
+                    for u_n, alpha_n in zip(u_k, alpha_k)])]
+        
+        # --- calcul du pire echantillon ------
+        def erreurs_inf(ensemble_echantillons):
+            x0 = []
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                x0 += list(executor.map(call_func_with_computed_data, 
+                    zip(ensemble_echantillons,
+                        repeat(erreur_norme_inf), repeat(Phi))))
+            return x0
+        x0_apprentissage = erreurs_inf(ensemble_apprentissage)
+        worst_elem_apprentissage = np.argmax(x0_apprentissage)
+        erreur_max_residuel += x0_apprentissage[worst_elem_apprentissage]
+
+        x0_test = erreurs_inf(ensemble_test)
+        worst_elem_test = np.argmax(x0_test)
+        erreur_max += x0_test[worst_elem_test]
+
+        def draw_one_profile(Phi, u, all_f, alpha, *param):
+            """ on va dessiner le PIRE profile"""
+
+            erreurs = np.array([max(abs(u_n - Phi @ alpha_n)) for u_n, alpha_n in zip(u, alpha)])
+            indice = np.argmax(erreurs)
+
+            plt.plot(discretisation_h, Phi @ alpha[indice], 'r')
+            plt.plot(discretisation_h, u[indice], 'g')
+            print("indice: ", indice, "erreur associee : ", erreurs[indice])
+
+
+        call_func_with_computed_data((ensemble_test[worst_elem_test], draw_one_profile, Phi))
+        export_img("hauteur(m)", "Phi alpha (K°)", "figures_optimisation/pire_profil_test_with_Phi"+str(i)+".png")
+
+        print("erreur (norme inf.): ", erreur_max[-1])
+        print("erreur residuelle (norme inf.): ", erreur_max_residuel[-1])
 except:
+    raise
     pass
 
-print(erreur_moyenne)
+print(erreur_max)
 rootgrp.close()
